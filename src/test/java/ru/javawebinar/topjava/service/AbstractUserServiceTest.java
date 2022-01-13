af3089246772cfd079b8ae006ac8fdd4064d8989
vsevolodgrigorev <sevagrigoryev@gmail.com>
2022-01-13 14:09:40 +0300

    optional - 3 fetch join

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -25,7 +25,7 @@ import static ru.javawebinar.topjava.UserTestData.*;
 public class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Autowired
-    private UserService service;
+    protected UserService service;
 
     @Autowired
     private CacheManager cacheManager;
